846a9d4a88299dde53b15beb99c0843cadc4d5fa
Patrick Fung <patrick@makestuffdostuff.com>
2019-08-23 17:38:03 -0400

    set position relative on the title container so the +/- indicator doesn't float off into the ether

--- rl-collapsible-section.php
+++ rl-collapsible-section.php
@@ -7,12 +7,12 @@ defined( 'ABSPATH' ) OR exit;
  * Author URI: https://github.com/ryersonlibrary
  * Description: Adds the [rl_collapsible_section] shortcode to WordPress.
  * GitHub Plugin URI: https://github.com/ryersonlibrary/custom-css-stylesheet-for-posts-or-pages
- * Version: 0.0.2
+ * Version: 0.0.3
  */
 
 function rl_collapsible_section_register_scripts() {
-  wp_register_style( 'rl-collapsible-section-style', plugin_dir_url( __FILE__ ).'/css/rl-collapsible-section.css', array(), '0.0.2' );
-  wp_register_script( 'rl-collapsible-section-js', plugin_dir_url( __FILE__ ).'/js/rl-collapsible-section.js', array('jquery'), '0.0.2', true );
+  wp_register_style( 'rl-collapsible-section-style', plugin_dir_url( __FILE__ ).'/css/rl-collapsible-section.css', array(), '0.0.3' );
+  wp_register_script( 'rl-collapsible-section-js', plugin_dir_url( __FILE__ ).'/js/rl-collapsible-section.js', array('jquery'), '0.0.3', true );
 }
 add_action( 'wp_enqueue_scripts', 'rl_collapsible_section_register_scripts' );
 
