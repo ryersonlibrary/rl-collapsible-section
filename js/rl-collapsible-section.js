c15c9120f3dc21528d1236da829a9c1a0430f6ef
Patrick Fung <patrick@makestuffdostuff.com>
2019-08-28 13:28:53 -0400

    fix bug where button was targeting the wrong div

--- js/rl-collapsible-section.js
+++ js/rl-collapsible-section.js
@@ -2,13 +2,16 @@ jQuery(document).ready( function( jQuery ){
   var _$ = jQuery; // jQuery
 
   var collapsibles = _$('.rl-collapsible-section');
+
   _$.each(collapsibles, function(index, collapsible) {
+    console.log(collapsible);
+
     _$collapsible = _$(collapsible);
     // toggle the content display when the title is clicked
     var titleButton = _$collapsible.find('.rl-collapsible-section-title button')[0];
+
     _$(titleButton).on('click', function() {
-      _$collapsible.toggleClass('rl-collapsed');
-      collapsible.toggleAttribute('collapsed');
+      _$(this).closest('.rl-collapsible-section').toggleClass('rl-collapsed');
     });
   });
 });
