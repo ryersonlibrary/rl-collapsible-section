2001d1a5e84ef90608b9ab1d9ab2f062ed40fc27
Patrick Fung <patrick@makestuffdostuff.com>
2019-08-28 13:02:18 -0400

    remove animations because they are a bit janky

--- js/rl-collapsible-section.js
+++ js/rl-collapsible-section.js
@@ -1,29 +1,14 @@
 jQuery(document).ready( function( jQuery ){
   var _$ = jQuery; // jQuery
 
-  // reset the explicitly defined height for all of our collapsible content divs
-  function setCollapsibleContentHeights() {
-    var collapsibleContents = _$('.rl-collapsible-section-content');
-    _$.each(collapsibleContents, function(index, content) {
-      _$(content).height( 'auto' );                    
-      _$(content).height( _$(content).outerHeight() );
-    });
-  }
-
   var collapsibles = _$('.rl-collapsible-section');
   _$.each(collapsibles, function(index, collapsible) {
-    var titleButton = _$(collapsible).find('.rl-collapsible-section-title button')[0];
-
+    _$collapsible = _$(collapsible);
     // toggle the content display when the title is clicked
+    var titleButton = _$collapsible.find('.rl-collapsible-section-title button')[0];
     _$(titleButton).on('click', function() {
-      _$(collapsible).toggleClass('rl-collapsed');
+      _$collapsible.toggleClass('rl-collapsed');
+      collapsible.toggleAttribute('collapsed');
     });
-  });
-
-  // explicitly define the height of the content so css transitions work 
-  // and recalculate content height when the window is resized
-  setCollapsibleContentHeights();
-  _$( window ).resize(function() {
-    setCollapsibleContentHeights();
   });
 });
