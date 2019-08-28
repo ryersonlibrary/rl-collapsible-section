a312094b3954fce0a0ce3c222c4a546adef82028
Patrick Fung <patrick@makestuffdostuff.com>
2019-08-28 13:29:07 -0400

    version bump

--- rl-collapsible-section.php
+++ rl-collapsible-section.php
@@ -7,7 +7,7 @@ defined( 'ABSPATH' ) OR exit;
  * Author URI: https://github.com/ryersonlibrary
  * Description: Adds the [rl_collapsible_section] shortcode to WordPress.
  * GitHub Plugin URI: https://github.com/ryersonlibrary/rl-collapsible-section
- * Version: 0.0.5
+ * Version: 0.0.6
  */
 
 function rl_collapsible_section_register_scripts() {
